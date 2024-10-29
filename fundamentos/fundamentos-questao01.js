c88a2e91e97de700a7285cee7d686f13ab055a84
jymimendes <108299587+jymimendes@users.noreply.github.com>
2024-10-28 23:12:16 -0300

    Delete fundamentos/fundamentos-questao01.js

--- fundamentos/fundamentos-questao01.js
+++
@@ -1,6 +0,0 @@
-let hoje = new Date();
-let diasSemana = ["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado"];
-let diaSemana = diasSemana[hoje.getDay()];
-let horaAtual = hoje.toLocaleTimeString();
-console.log(`Hoje é: ${diaSemana}.`);
-console.log(`A hora atual é: ${horaAtual}.`);
